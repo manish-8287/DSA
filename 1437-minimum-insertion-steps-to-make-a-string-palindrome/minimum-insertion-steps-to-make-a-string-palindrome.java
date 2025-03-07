e985ef3f6e7e3b4aae4dceaf046e4f27b6d5ac33
manish-8287 <144647018+manish-8287@users.noreply.github.com>
2025-03-07 22:39:07 +0530

    Time: 19 ms (86.70%) | Memory: 45.3 MB (85.58%) - LeetSync

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -0,0 +1,26 @@
+class Solution {
+    int func(String s , int start , int end,int[][] dp)
+    {
+        if(start>end) return 0;
+        // if(start==end) return 0;
+        if(dp[start][end]!=-1) return dp[start][end];
+        int count = 0;
+        if(s.charAt(start)==s.charAt(end))
+        {
+           return dp[start][end] = func(s , start+1 , end-1,dp);
+        }
+        else
+        {
+            dp[start][end]= Math.min(1+func(s , start+1 , end,dp) , 1+func(s, start,end-1,dp));
+        }
+        return dp[start][end];
+    }
+    public int minInsertions(String s) {
+        int[][] dp  = new int[s.length()+1][s.length()+1];
+        for(int[] i : dp)
+        {
+            Arrays.fill(i,-1);
+        }
+        return func(s,0,s.length()-1,dp);
+    }
+}
