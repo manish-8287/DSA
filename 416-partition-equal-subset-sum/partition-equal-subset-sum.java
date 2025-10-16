3f2535a577009d122a07aa03bdc178a3123445e9
Manish Kumar <144647018+manish-8287@users.noreply.github.com>
2025-10-16 13:45:12 +0530

    Time: 48 ms (49.46%) | Memory: 48.1 MB (46.55%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -5,29 +5,42 @@ class Solution {
         {
             sum += nums[i];
         }
-        Boolean[][] dp = new Boolean[nums.length+1][sum+1];
-        if(sum % 2 == 0)
+        if(sum % 2 != 0 ) return false;
+        int target = sum/2;
+        boolean[][] dp = new boolean[nums.length+1][target+1];
+        for(int i=0;i<nums.length+1;i++)
         {
-            return func(nums , sum/2 , 0 , dp);
+            dp[i][0] = true;
         }
-        else
+        for(int i=1;i<nums.length+1;i++)
         {
-            return false;
+            for(int j=1 ; j<target+1 ; j++)
+            {
+                if(nums[i-1] <= j)
+                {
+                    dp[i][j] = dp[i-1][j - nums[i-1]] || dp[i-1][j];
+                }
+                else
+                {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
         }
+        return dp[nums.length][target];
     }
-    public static boolean func(int[] arr , int sum , int idx , Boolean[][] dp )
-    {
-        if(sum == 0) return true;
-        if(idx == arr.length) return false;
-        if(dp[idx][sum] != null) return dp[idx][sum];
+    // public static boolean func(int[] arr , int sum , int idx , Boolean[][] dp )
+    // {
+    //     if(sum == 0) return true;
+    //     if(idx == arr.length) return false;
+    //     if(dp[idx][sum] != null) return dp[idx][sum];
 
-        if(arr[idx] <= sum)
-        {
-            return dp[idx][sum] = func(arr , sum - arr[idx] , idx+1 , dp) || func(arr , sum , idx+1,dp);
-        }
-        else
-        {
-            return dp[idx][sum] = func(arr , sum , idx+1 , dp);
-        }
-    }
+    //     if(arr[idx] <= sum)
+    //     {
+    //         return dp[idx][sum] = func(arr , sum - arr[idx] , idx+1 , dp) || func(arr , sum , idx+1,dp);
+    //     }
+    //     else
+    //     {
+    //         return dp[idx][sum] = func(arr , sum , idx+1 , dp);
+    //     }
+    // }
 }
